c86a3d2f36e1feba008a65e88d46dd7f79a18ace
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-01 19:06:12 -0300

    added webmock gem debbug auto rspec files and added associations on user model rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations ' do
+    it {should have_many(:orders)}
+    it {should have_many(:user_payments)}
+    it {should have_many(:user_addresses)}
+    
+  end
 end
