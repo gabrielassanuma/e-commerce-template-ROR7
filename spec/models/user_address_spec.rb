614cb059dba04dd75379ae02bfa2b980be0040d2
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-06 17:57:09 -0300

    useraddresses model passed rspec tests

--- spec/models/user_address_spec.rb
+++ spec/models/user_address_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe UserAddress, type: :model do
   describe 'associations' do
-    it {should belongs_to(:user)}
+    it {should belong_to(:user)}
   end
 
   describe 'validations' do 
