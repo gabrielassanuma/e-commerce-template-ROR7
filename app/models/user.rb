bd88334e5b31f199b33b780d90805ef79b120a01
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-06 17:11:13 -0300

    user model passed all rspec validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+  has_many :orders
+  has_many :user_payments
+  has_many :user_addresses
+  validates :phone_number, length: { is: 9 }, uniqueness: true
+  validates :password, length: { in: 6..20 }
+  validates :first_name, :last_name, presence: true
 end
