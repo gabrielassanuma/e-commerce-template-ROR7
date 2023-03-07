a3d89dfce45374a50f23e225225f9047466d9f57
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-07 10:01:03 -0300

    order model setup

--- db/migrate/20230307124111_create_orders.rb
+++ db/migrate/20230307124111_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
-      t.float :total_price
+      t.float :total_price, null: false, default: 0.0
 
       t.timestamps
     end
