0ab365de2146308812cf559b879ca78cef0cc757
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-09 09:29:51 -0300

    debug enum user migration and changed factorybot user

--- db/migrate/20230309120240_add_enumrole_to_user.rb
+++ db/migrate/20230309120240_add_enumrole_to_user.rb
@@ -2,7 +2,7 @@ class AddEnumroleToUser < ActiveRecord::Migration[7.0]
   def change
     create_enum :role, ['user', 'admin']
 
-    change_table :orders do |t|
+    change_table :users do |t|
       t.enum :user, enum_type: 'role', default: 'user', null: false
     end
 
