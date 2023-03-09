149d167a7b97071d1a123ac30ddcf03317e4921f
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-09 16:09:15 -0300

    set up factory bot user_payment

--- spec/factories/user_payments.rb
+++ spec/factories/user_payments.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user_payment do
-    user { nil }
-    card_name { "MyString" }
-    card_number { "MyString" }
-    card_expiration { "MyString" }
+    association :user
+    card_name { Faker::Name.name }
+    card_number { Faker::Number.number(digits: 16) }
+    card_expiration { Faker::Number.number(digits: 4) }
   end
 end
