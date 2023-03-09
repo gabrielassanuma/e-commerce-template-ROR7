b9fb9b5257c0810c03da9b7847d4e9b107b298c4
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-09 09:01:15 -0300

    created factory bot for rspec user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    role { "user" }
+    first_name { Faker::Name.first_name   }
+    last_name { Faker::Name.last_name  }
+    email { Faker::Internet.email }
+    phone_number { Faker::Number.number(digits: 9) }
   end
 end
