e412a67e8afe34715c26905e37fa2c8833e6d239
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-12 11:45:15 -0300

    rspec model discounts

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Discount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it {should belong_to(:product)}
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:discount)}
+  end
 end
