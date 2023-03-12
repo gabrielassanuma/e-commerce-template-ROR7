20e59fce37e47bdfac7b021c76dcd769f90ca0ba
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-12 17:48:22 -0300

    added rspec model orderproduct

--- spec/models/order_product_spec.rb
+++ spec/models/order_product_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe OrderProduct, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'associations' do
+    it { should belong_to(:order).optional }
+    it { should belong_to(:cart) }
+    it { should belong_to(:product) }
+  end
+
+  def optional(optional = true)
+    remove_submatcher(AssociationMatchers::RequiredMatcher)
+    add_submatcher(
+      AssociationMatchers::OptionalMatcher,
+      name,
+      optional,
+    )
+    self
+  end
 end
