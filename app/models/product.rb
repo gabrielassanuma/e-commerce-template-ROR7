5612d8ef484752569005589d81e1382a2b64cf98
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-14 16:06:08 -0300

    changes on rspec products and model passed rspec

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :discount
+  has_many :order_products
+
+  validates :name, :description, :price, :active, presence: true
 end
