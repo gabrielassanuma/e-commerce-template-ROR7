83bb43870c7e08b2919184fa902eeaa6247157f6
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-14 16:17:24 -0300

    model cart passed rspec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   describe "associations" do
-    it { should have_many(:order_products) }
-    it { should have_many(:products) }
+    it { should belong_to(:user) }
   end
 end
