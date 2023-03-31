11c60a7f950460940de6d46d4923c7bff12c0bbd
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-31 11:19:45 -0300

    change root to products index, set before action to show for all users, implement front end to products index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/home'
   get 'about', to: 'pages#about', as: 'about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "products#index"
   resources :products, except: [:destroy]
 end
