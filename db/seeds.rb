57411db78f68e8cb52a1208855c08a1807b868e0
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-04-03 10:09:56 -0300

    added users addresses seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@ puts "====== Creating Users ========"
 admin = User.new(email: "admin@admin.com", first_name: "admin", last_name:"admin", phone_number: "000000000", password: 111111, password_confirmation: 111111, role: "admin")
 admin.save!
 puts "====== Admin created ========="
-9.times do 
+users = []
+10.times do 
   user = User.new(
     first_name: Faker::Name.first_name, 
     last_name: Faker::Name.last_name, 
@@ -18,5 +19,21 @@ puts "====== Admin created ========="
     role: "user"
   )
   user.save!
+  users << user 
 end
 puts "====== Users created ========="
+
+puts "=== Creating User's address ==="
+users.each do | user |
+  user_address = UserAddress.new(
+    name: ["home", "job", "school"].sample,
+    address: Faker::Address.full_address,
+    complement: ["", "", "backyard", ""].sample,
+    user: user,
+    main: true
+  )
+  user_address.save!
+
+end 
+puts "=== User's address created ===="
+
