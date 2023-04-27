9684903f1bde20d10e5b722c62d0ca33ffdd2930
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-04-27 16:19:27 +0100

    created discounts controller and created trait invalid for factory bot

--- spec/factories/discounts.rb
+++ spec/factories/discounts.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :discount do
     name { "sale" }
     discount { Faker::Number.between(from: 0.0, to: 15.0)}
+
+    trait :invalid do
+      name {""}
+    end
   end
 end
