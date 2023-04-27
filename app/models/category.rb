fd7ae20ed5681a10b7d0bafd1ce5725c20dd2a49
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-04-27 12:21:51 +0100

    changed model and logic on creation of new category to applu sortable

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,14 @@
 class Category < ApplicationRecord
   has_many :products
 
-  validates :name, :position, presence: true
+  validates :name, presence: true
+  validates :position, uniqueness: true
 
   def deactive!
     update(active: false)
+  end
+
+  def self.last_position
+    Category.order(position: :desc).first&.position || 0
   end
 end
