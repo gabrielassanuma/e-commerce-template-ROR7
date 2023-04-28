961565576342c73542d58a67e525646a0869cd88
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-04-28 14:30:32 +0100

    set routes move to use on sortable

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -1,11 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 import Sortable from 'sortablejs'
 
+
 // Connects to data-controller="drag"
 export default class extends Controller {
   connect() {
     console.log("connected");
-    console.log(Sortable); 
     this.sortable = Sortable.create(this.element, {
       onEnd: this.end.bind(this)
     })
